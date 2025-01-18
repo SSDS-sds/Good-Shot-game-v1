4f2015c54a91127451d7b65488124554883d96ab
SSDS-sds <warwicklee1@gmail.com>
2025-01-18 18:21:54 +0000

    Add files via upload

--- game.py
+++ game.py
@@ -12,13 +12,21 @@ alien = Actor("alien")
 
 def draw():
     screen.clear()
-    screen.fill(color = ("#7F95D1"))
+    screen.fill(color = ("blue"))
     alien.draw()
     screen.draw.text(msg,center = (400,20),fontsize = 30,color = ("#0A210F"))
 
 def place_aln ():
     alien.x = randint(50,450)
     alien.y = randint(50,450)
+
+def on_mouse_down(pos):
+    global msg
+    if alien.collidepoint(pos):
+        msg = "GOOD SHOT!"
+        place_aln()
+    else:
+        msg = "YOU MISSED!!!"
 
 place_aln()
 
